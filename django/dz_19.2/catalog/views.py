b31d8b6527dd3efafa893d469566252150c9129a
MarakovR <133142896+MarakovR@users.noreply.github.com>
2023-10-01 12:24:20 +0300

    Delete django directory

--- django/dz_19.2/catalog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'catalog/index.html')
-
-
-def views_contacts(request):
-    return render(request, 'catalog/contacts.html')
